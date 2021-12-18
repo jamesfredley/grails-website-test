1f749a387651c84a42c4bf94d53521dfb5b43a40
Scott Murphy <scott@alwaysvip.com>
2021-12-17 22:36:04 -0800

    init script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mv website website-old
+grails create-app website
+cd website
+mv ../website-old/.git .
+git checkout README.md
+grails create-domain-class Sample
+# add 3 properties to Sample.groovy     String prop1, Integer prop2, Date prop3
+git checkout grails-app/domain/website/Sample.groovy
+grails generate-controller website.Sample 
+grails generate-views website.Sample 
+grails install-templates 
