0610e23389cecc18439a7308ff892ccdddcf1a12
Scott Murphy <scott@alwaysvip.com>
2023-07-25 16:26:04 -0700

    grails 6 support

--- setup6.sh
+++ setup6.sh
@@ -6,8 +6,8 @@ git checkout README.md
 grails create-domain-class Sample
 # add 3 properties to Sample.groovy     String prop1, Integer prop2, Date prop3
 git checkout grails-app/domain/website/Sample.groovy
-grails generate-controller website.Sample 
-grails generate-views website.Sample 
-grails install-templates
-git checkout setup.sh
-git checkout setup6.sh
+./gradlew runCommand "-Pargs=generate-controller website.Sample"
+./gradlew runCommand "-Pargs=generate-views website.Sample"
+./gradlew runCommand "-Pargs=install-templates"
+git checkout setup.sh 
+git checkout setup6.sh 
